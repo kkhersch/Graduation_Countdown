03e2cf2d368a073952f4c0e3641fd96fea24a143
Ted George <tdzilla57@gmail.com>
2019-04-14 22:21:33 -0700

    Create 2 basic unit tests in functionality.rb
    References #3

--- spec/functionality.rb
+++ spec/functionality.rb
@@ -8,4 +8,12 @@ require 'rspec/rails'
 Rspec.describe Example do
   it "is an example test with no functionality" do
   end
+
+  it "has a value to convert minutes to hotdogs eaten" do
+    expect(hotDogsConverter).to be == 7.2
+  end
+
+  it "has a value to convert minutes to steam games released" do
+    expect(steamGamesReleasedConverter).to be == .0146
+  end
 end
