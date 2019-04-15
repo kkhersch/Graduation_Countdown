a3e9e54bde2180f9e281ab5402c769c3ca1e809d
Rasheed <relkassed@gmail.com>
2019-04-14 22:01:49 -0700

    Add basic test file

---
+++ spec/functionality.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+Rspec.describe Example do
+  it "is an example test with no functionality" do
+  end
+end
