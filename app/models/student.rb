e74638dafcf817642d71cbc65e462bcce3ab33ea
Rasheed <relkassed@gmail.com>
2019-04-22 05:48:19 -0700

    Add basic credits system

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,37 @@
 class Student
   def initialize
+    @days_until_graduation = 0
     @declared_major = "No defined major"
+    @credits = 0;
+  end
+  def set_days_until_graduation(days)
+    @days_until_graduation = days
   end
   def days_until_graduation
-    0
+    @days_until_graduation
   end
   def declared_major
     @declared_major
   end
   def set_major(major)
     @declared_major = major
+  end
+  def class_standing
+    if(@credits < 50)
+      "Freshman"
+    elsif (@credits < 90)
+      "Sophmore"
+    elsif (@credits < 135)
+      "Junior"
+    else 
+      "Senior"
+    end
+  end
+  def set_credits(credits)
+    if(credits < 0)
+      raise ArgumentError,"Positive credits value expected"
+    else
+      @credits = credits
+    end
   end
 end
--- spec/students_spec.rb
+++ spec/students_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 describe "Student" do
-  it "has a graduation date" do
+  it "has a changeable graduation date" do
     fake_student = Student.new
     expect(fake_student.days_until_graduation).to be_a (Integer)
     expect(fake_student.days_until_graduation).to be >= 0
+    fake_student.set_days_until_graduation(400)
+    expect(fake_student.days_until_graduation).to be == 400
   end
   it "has a declared major" do
     fake_student = Student.new
@@ -14,5 +16,15 @@ describe "Student" do
   it "handles a lack of a major gracefully" do
     fake_student = Student.new
     expect(fake_student.declared_major).to be == "No defined major"
+  end
+  it "has a class standing based on credits" do
+    fake_student = Student.new
+    expect(fake_student.class_standing).to be == "Freshman"
+    fake_student.set_credits(150)
+    expect(fake_student.class_standing).to be == "Senior"
+  end
+  it "does not allow negative credit values" do
+    fake_student = Student.new
+    expect{fake_student.set_credits(-10)}.to raise_error("Positive credits value expected")
   end
 end
