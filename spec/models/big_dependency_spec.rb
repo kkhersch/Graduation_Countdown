be9368181da4d262cd649d49e1d554520daa5dcd
Rasheed <relkassed@gmail.com>
2019-05-19 22:26:21 -0700

    Fix typos

--- spec/models/big_dependency_spec.rb
+++ spec/models/big_dependency_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "test the large method" do
-  let (:fake_multiplier) {FactoryBot.build_stubbed(:multiplier, :Steam)}
+  let (:fake_multiplier) {FactoryBot.build_stubbed(:multiplier, :steam)}
 
   it "can run the large method using a fake" do
     result = fake_multiplier.long_method(FakeBigDependency.new)
@@ -9,7 +9,7 @@ RSpec.describe "test the large method" do
   end
 
   it "can run the large method with a mock / stub" do
-    allow(fake_multiplier).to recieve(:long_method).with(7).and_return(7)
+    allow(fake_multiplier).to receive(:long_method).with(7).and_return(7)
     result = fake_multiplier.long_method(7)
     expect(result).to eq(7)
   end
--- spec/models/determine_time_spec.rb
+++ spec/models/determine_time_spec.rb
@@ -1,59 +1,57 @@
 require "rails_helper"
 
-Rspec.describe "determine time"
+RSpec.describe "determine time", type: :model do
   let (:freshman_student) {FactoryBot.create(:student, :freshman)}
   let (:sophmore_student) {FactoryBot.create(:student, :sophmore)}
   let (:junior_student) {FactoryBot.create(:student, :junior)}
   let (:senior_student) {FactoryBot.create(:student, :senior)}
   
-  RSpec.describe "is possible", type: :model do
-    it "Has a class standing" do
-      expect(freshman_student.class_standing).to eq("Freshman")
-    end
-
-    it "Has a class standing based on credits" do
-      expect(sophmore_student.class_standing).to eq("Sophmore")
-    end
-
-    it "Can determine time based on class standings" do
-      expect(sophmore_student.caluclate_time_until_graduation).to eq(1095)
-    end
-
-    it "Has a time value for every class standing" do
-      expect(freshman_student.caluclate_time_until_graduation).to eq(1460)
-      expect(sophmore_student.caluclate_time_until_graduation).to eq(1095)
-      expect(junior_student.caluclate_time_until_graduation).to eq(730)
-      expect(senior_student.caluclate_time_until_graduation).to eq(365)
-    end
+  it "Has a class standing" do
+    expect(freshman_student.class_standing).to eq("Freshman")
   end
 
-  RSpec.describe "is calculable" do
-    let(:fake_multiplier) {FactoryBot.build_stubbed(:multiplier, :steam)}
-    let(:fake_multiplier_float) {FactoryBot.build_stubbed(:multiplier, :rupie)}
-    let(:fake_multiplier_default) {FactoryBot.build_stubbed(:multiplier, :default)}
+  it "Has a class standing based on credits" do
+    expect(sophmore_student.class_standing).to eq("Sophmore")
+  end
 
-    it "Has a major" do
-      fake_student = Student.create(declared_major: "Computer Science")
-      expect(fake_student.declared_major).to eq("Computer Science")
-    end
+  it "Can determine time based on class standings" do
+    expect(sophmore_student.caluclate_time_until_graduation).to eq(1095)
+  end
 
-    it "Has a multiplier value that is not an integer" do
-      expect(fake_multiplier_float.multiplier_value).to eq(5.7)
-    end
+  it "Has a time value for every class standing" do
+    expect(freshman_student.caluclate_time_until_graduation).to eq(1460)
+    expect(sophmore_student.caluclate_time_until_graduation).to eq(1095)
+    expect(junior_student.caluclate_time_until_graduation).to eq(730)
+    expect(senior_student.caluclate_time_until_graduation).to eq(365)
+  end
+end
 
-    it "Can select a multiplier based off of major" do
-      fake_student = Student.create(declared_major: "Computer Science")
-      fake_multiplier_default.retrieve(fake_student.declared_major)
-      expect(fake_multiplier_default.name).to eq("Games Released on Steam")
-      expect(fake_multiplier_default.multiplier_value).to eq(21)
-    end
+RSpec.describe "Calculates final output" do
+  let(:fake_multiplier) {FactoryBot.build_stubbed(:multiplier, :steam)}
+  let(:fake_multiplier_float) {FactoryBot.build_stubbed(:multiplier, :rupie)}
+  let(:fake_multiplier_default) {FactoryBot.build_stubbed(:multiplier, :default)}
 
-    it "Can calculate time till graduation * multiplier" do
-      fake_student = Student.create(declared_major: "Computer Science", credits: 72)
-      fake_multiplier_default.retrieve(fake_student.declared_major)
-      time_value = fake_student.caluclate_time_until_graduation
-      result = fake_multiplier_default.multiplier_value * time_value
-      expect(result).to eq(22995)
-    end
+  it "Has a major" do
+    fake_student = Student.create(declared_major: "Computer Science")
+    expect(fake_student.declared_major).to eq("Computer Science")
+  end
+
+  it "Has a multiplier value that is not an integer" do
+    expect(fake_multiplier_float.multiplier_value).to eq(5.7)
+  end
+
+  it "Can select a multiplier based off of major" do
+    fake_student = Student.create(declared_major: "Computer Science")
+    fake_multiplier_default.retrieve(fake_student.declared_major)
+    expect(fake_multiplier_default.name).to eq("Games Released on Steam")
+    expect(fake_multiplier_default.multiplier_value).to eq(21)
+  end
+
+  it "Can calculate time till graduation * multiplier" do
+    fake_student = Student.create(declared_major: "Computer Science", credits: 72)
+    fake_multiplier_default.retrieve(fake_student.declared_major)
+    time_value = fake_student.caluclate_time_until_graduation
+    result = fake_multiplier_default.multiplier_value * time_value
+    expect(result).to eq(22995)
   end
 end
