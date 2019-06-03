55e6795e8c5f00584ad37231ffe2f594367ad69b
Florentina Ilie <ilief@oregonstate.edu>
2019-06-03 11:58:31 -0700

    ADDED TEST FOR MULTIPLIER NAME

--- spec/models/multiplier_spec.rb
+++ spec/models/multiplier_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe Multiplier, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   let (:fake_multiplier) {FactoryBot.create(:multiplier, :steam)}
-  # it "has a changeable graduation date" do
-  #   fake_student.days_until_graduation = 400
-  #   expect(fake_student.days_until_graduation).to be == 400
-  # end
+  it "has a multiplier name" do
+    fake_multiplier.name = "Games Released on Steam"
+    expect(fake_multiplier.name).to eq('Games Released on ')
+  end
 end
