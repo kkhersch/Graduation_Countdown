4f510e8ff08846953669922043eca73decf1d1ed
Florentina Ilie <ilief@oregonstate.edu>
2019-06-03 11:45:10 -0700

    REFACTORED NEW STUDENTS WEB VIEW AND STUBBED TEST FOR MULTIPLIER RETRIEVE METHOD.

--- spec/models/multiplier_spec.rb
+++ spec/models/multiplier_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-# RSpec.describe Multiplier, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Multiplier, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  let (:fake_multiplier) {FactoryBot.create(:multiplier, :steam)}
+  # it "has a changeable graduation date" do
+  #   fake_student.days_until_graduation = 400
+  #   expect(fake_student.days_until_graduation).to be == 400
+  # end
+end
