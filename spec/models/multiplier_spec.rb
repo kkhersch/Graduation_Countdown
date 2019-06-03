b6a5c5c7060f6771438386ca4aadb4074ff1100e
Florentina Ilie <ilief@oregonstate.edu>
2019-06-03 12:03:23 -0700

    ADDED TEST FOR MULTIPLIER VALUE.

--- spec/models/multiplier_spec.rb
+++ spec/models/multiplier_spec.rb
@@ -4,7 +4,9 @@ describe Multiplier, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   let (:fake_multiplier) {FactoryBot.create(:multiplier, :steam)}
   it "has a multiplier name" do
-    fake_multiplier.name = "Games Released on Steam"
-    expect(fake_multiplier.name).to eq('Games Released on ')
+    expect(fake_multiplier.name).to eq('Games Released on Steam')
+  end
+  it "has a multiplier value" do
+    expect(fake_multiplier.multiplier_value).to eq 21
   end
 end
