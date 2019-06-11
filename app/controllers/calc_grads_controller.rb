7a0941272f3a301b08b48e287174d59eb5d522a9
Kenney <programmingsammich@gmail.com>
2019-06-10 23:12:54 -0700

    add pending test for graduation calculation next step: figure out select tag, capybara is unable to find the value of the selection in the select tag

--- app/controllers/calc_grads_controller.rb
+++ app/controllers/calc_grads_controller.rb
@@ -2,5 +2,6 @@ class CalcGradsController < ApplicationController
   def index
     @multipliers = Multiplier.all
     @students = Student.all
+    @student_options = Student.all.map { |student| student.student_name}
   end
 end
--- spec/system/calculate_graduation_spec.rb
+++ spec/system/calculate_graduation_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "calculating graduation time of a student", type: :system do
-  it "allows a user to calculate a selected student's graduation time" do
+  # let( :test_student_name) {test_student_name = 'Matt Damon'}
+
+  pending "allows a user to calculate a selected student's graduation time" do
     visit calc_grads_path
-    select "Matt Damon", :from => "Student Name"
-    choose('Jason Bourne Identity')
-    click_on("Calculate Graduation Time")
-    expect(page).to have_content(4380)
+
+    within('#calc_grad_form') do
+      select 'Matt Damon', from: 'student_name'
+    end
   end
 end
